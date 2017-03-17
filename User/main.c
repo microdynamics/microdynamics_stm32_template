3889c57bc5d43ad31edf6cdc36f877dec257a121
myyerrol <myyerrol@126.com>
2017-03-17 21:20:39 +0800

    Add the drivers and modules

--- User/main.c
+++ User/main.c
@@ -1,30 +1,18 @@
-#include "stm32f10x.h"
-
-#define TIME 3000000
-
-void delayTime(u32 count)
-{
-    u32 i = 0;
-
-    for (; i < count; i++);
-}
+#include "stm32f10x_driver_delay.h"
+#include "stm32f10x_module_led.h"
 
 int main(void)
 {
-    GPIO_InitTypeDef GPIO_InitStructure;
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOE, ENABLE);
-    GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_5;
-    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
-    GPIO_SetBits(GPIOB, GPIO_Pin_5);
+    Delay_Init();
+    LED_Init();
 
     while (1)
     {
-        GPIO_ResetBits(GPIOB, GPIO_Pin_5);
-        delayTime(TIME);
-        GPIO_SetBits(GPIOB, GPIO_Pin_5);
-        delayTime(TIME);
+        LED0 = 0;
+        Delay_TimeMs(5000);
+        LED0 = 1;
+        Delay_TimeUs(5000);
     }
+
+    return 0;
 }
